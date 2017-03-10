dd96db2dfda2c7a0de605cb7b3bc13039fd41895
GichungeKelvin <kimgichunge6@gmail.com>
2017-03-10 01:06:04 -0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE htmll>
+<!--Web Project Developed in a Hostel by Gichunge Kelvin Kimathi in Multimedia University of Kenya -->
+<HTML lang="en">
+<HEAD>
+  <TITLE>KelvinGichunge_Services</TITLE>
+  <meta name="description" content="Online Portfolio for Gichunge Kelvin; a Science and Technology Enthusiast">
+  <meta name="keywords" content="web designer,web developer,for hire,social media marketing,Gichunge Kelvin Kimathi,Software Developer Africa Kenya">
+  <meta name="author" content="Gichunge Kelvin">
+  <meta charset="utf-8">
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> 
+  <script src="jquery/jquery.min.js"></script> 
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="Stylesheets/Styles.css">
+  <style>
+    .activated_link{ color: #000000; background: #ff3333; }
+  </style>
+  <script>
+    $(document).ready(function(){
+      $('#home_link').addClass('activated_link');     
+    })
+  </script>
+</HEAD>
+<BODY>   
+    <div id = 'top_header' class='container-fluid' >
+      <h4><b>&lt; &frasl;<img src="Images/book_icon.ico" height='30'>kelvinGichunge_ &gt;</b><a name='top_home'></a></h4>
+      <h6><b>WITH SOFTWARE CREATION, ALL THE WORLD'S PROBLEMS ARE SOLVED !</b></h6>
+    </div>
+    <!--Top Menu-->    
+    <div  class='menu' data-spy="affix" data-offset-top="80">
+        <ul>
+           <li><a href="Index.php#top_home" id='home_link'>Home</a></li>
+           <div class='dropdown'>
+           <li><a href="#">What Services Do I Offer? &#9662;</a>              
+                 <ul class='dropdown-menu'>
+                      <li><a href="#web_design">Web Design</a></li>
+                      <li><a href="#web_development">Web Development</a></li>
+                      <li><a href="#software_dev">Desktop App Development</a></li>
+                      <li><a href="#social_media_marketing">Social Media Marketing</a></li>
+                  </ul>               
+           </li>
+           </div>
+           <li><a href="Pages/Hire_services.php" id='Links'>Hire Me</a></li>
+           <li><a href="Pages/Resume.php" id='Links'>My Resume</a></li>
+           <li><a href="Pages/About.php" id='Links'>Who am I?</a></li>
+           <li><a href="Pages/Message.php" id='Links'>Send Message to Me</a></li>
+           <li ><a href="Pages/Projects.php" id='Links'>Completed Projects</a></li>              
+        </ul>
+    </div> 
+        
+  <div class='top_advert'>
+    <img src="Images/Banner0.jpg" id='img' alt='Software_Programmer'>    
+    <div class='container-fluid'>
+      <div> 
+        <h2 id='header_font'>Do You Require A Web Designer,Web Developer or Desktop Software Developer?</h2> 
+        <h3>Worry Not,I Am Here For You!...</h3> 
+      </div>  
+    </div><br>
+    <div class='container'>
+    <table class='table-responsive'>
+      <thead>
+        <tr>
+          <td><h3><u><b>Web Design</b></u></h3></td>
+           <td><h3><u><b>Web Development</b></u></h3></td>
+            <td><h3><u><b>Desktop Software Development</b></u></h3></td>
+        </tr>
+        <tr>
+          <td>
+            <ul class='list'>
+              <li>Design Beautiful Webpage Layouts</li>
+              <li>Compatible Colors $ Layers</li>
+              <li>Attractive Headers</li>
+              <li>Marketable Landing Pages</li>
+              <li>Edit and Maintain Already made Pages</li>
+              <li>Upload Website To The Hosting-Server</li>
+              <li>Web SEO</li>
+              <li>More...</li>
+            </ul>
+          </td>
+          <td>
+            <ul class='list'>
+              <li>Create and Link to Databases</li>
+              <li>Edit Server-Side Language</li>
+              <li>Enable Security to the Databases</li>
+              <li>Upload Website To The Hosting-Server</li>
+              <li>Web SEO</li>
+              <li>More...</li>
+            </ul>
+          </td>
+          <td>
+            <ul class='list'>
+              <li>Windows Console Applicatins in Python</li> 
+              <li>Windows Console Applications in Batch Script</li> 
+              <li>Task Automation in Batch Script & Python</li>                           
+            </ul>
+          </td>
+        </tr>
+      </thead>
+    </table>     
+  </div>
+</div>
+<div class='web_design'><br>
+  <h2>Services That I Offer</h2><br><br>  
+  <h3 id='h3_design'><b>Web Design</b><a name='web_design'></a></h3>
+  <img src="Images/Banner6.jpg" id='img' class='web_services' alt='Web_Designer'>
+</div>
+<div id='container_design' >  
+  <div class='container' >
+  <h2 id='design'><b>Web Design:</b></h2>
+  <h2 id='design_2'><b>Creating Attractive</b> Customer/User Interfaces</h2>
+  <h3 id='design_2'>That Eventually leads to <b>Attracting more Customers</b> and <b>People</b>,thus;</h3>
+  <h3 id='design_2'>Leading to <b>Marketing</b> and <b>Increase in Sales</b> of a Company and <b>Popularity</b> of a Site</h3><br>
+  <h4 id='design_3'>In <b>Web Design</b>, I Use The Following Programming Languages :</h4>
+  <ul class='design'>
+    <li>HTML5</li>
+    <li>CSS & BOOTSTRAP</li>
+    <li>JAVASCRIPT,JQUERY & ANGULAR-JS</li>
+    <li>AJAX</li>
+  </ul><br>
+  <a href="Pages/Hire_services.php" class="btn btn-success btn-lg" role="button">Hire Me (Web Designer)</a><br><br><br>
+  <a href="#top_home" class='top'><b>Back to Top</b></a><br>
+  </div>
+</div>
+  <div><br><br><hr>
+    <h3 class='development'><b>Web Development</b><a name='web_development'></a></h3>
+    <img src="Images/Banner7.jpg" id='img' alt='Web_Developer'>
+  </div> 
+  <div class='container'>
+    <h2 id='dev'><b>Web Development:</b></h2>
+    <h3 id='design_2'>Using Server Side Language to</h3>
+    <h3 id='design_2'>make and Connect to <b>Databases</b> to <b>Enable Storage</b> and <b>Management</b> of</h3>
+    <h3 id='design_2'><b>Customer's</b> and <b>Company/Institution Records</b></h3><br>
+    <h4 id='design_3'>In <b>Web Development</b>, I Use The Following Programming Languages :</h4>
+  <ul class='design'>
+    <li>PHP & MYSQL</li>
+    <li>SQL</li>    
+    <li>AJAX</li>
+    <li>JSON</li>
+  </ul><br>
+  <a href="Pages/Hire_services.php" class="btn btn-success btn-lg" role="button">Hire Me (Web Developer)</a><br><br><br>
+  <a href="#top_home" class='top'><b>Back to Top</b></a>
+  </div><br><br>
+  <div class='dsk_dev'>
+    <h3 class='dsktp_dev'><b>Desktop Software Development</b><a name='software_dev'></a></h3>
+    <img src="Images/Banner2.jpg" id='img' alt='Software_Programmer'>
+  </div>
+  <div class='container-fluid' id='soft_dev'>
+    <div class='container'>
+          <h3 id='design_2'>In <b>Desktop Software Programming</b>;</h3>
+          <h4 id='design_2'>I <b>Create Console</b> Applications and</h4>
+          <h4 id='design_2'><b>Automate Windows</b> Tasks with Batch Scripting</h4><br>
+          <h4 id='design_4'>In <b>Desktop Software Programming</b>, I Use The Following Programming Languages :</h4>
+          <ul id='design_4'>
+            <li><b>Python (Console Applications)</b></li>
+            <li><b>Batch Scripting (Task Automation)</b></li>
+          </ul><br>
+          <a href="Pages/Hire_services.php" class="btn btn-success btn-lg" role="button">Hire Me (Software Programmer)</a><br><br><br> 
+          <a href="#top_home" class='top'><b>Back to Top</b></a> <br><br><br>     
+    </div><br>   
+    <h3 class='dsktp_dev'><b>Social Media & Email Marketing</b><a name='social_media_marketing'></a></h3>
+    <img src="Images/Banner10.jpg" id='img' alt='Social_media_marketer'>
+  </div> 
+  <div class='container-fluid' id='soft_dev'>
+     <h3 id='design_2'><b>Social Media & Email Marketing Involves</b>;</h3>
+     <div class='container' >
+       <ul>        
+        <h4><li><b>Targeting</b> the Right <b>Customers</b> and <b>Directing them to your Landing Pages</b></li></h4>
+        <h4><li><b>Gathering Emails</b> from your Right Customers, to be used in <b>Product Promotion</b></h4>
+        <h4><li><b>Promoting</b> Your <b>Services</b> and <b>Items</b> on <b>Social Sites</b> and <b>Emails</b></li></h4>
+        <h4><li><b>Creating</b> Extra <b>Blogs</b> to Advertise your main Website,Services,Items</li></h4>
+        <h4><li>Creating & Customizing <b>Email Templates</b> to be used in <b>Advertising</b></li></h4>
+        <h4><li>Reaching Out for Affliates incase you need them</li></h4> 
+        <h4><li>More...</li></h4>       
+       </ul><br>       
+     </div> 
+      <a href="Pages/Hire_services.php" class="btn btn-success btn-lg" role="button">Hire Me (Social Media Marketing)</a><br><br><br>  
+      <a href="#top_home" class='top'><b>Back to Top</b></a> <br><br><br><br><br><br>     
+  </div>
+   <div id = 'footer'>    
+        <h5>Copyright &copy;<script>document.write(new Date().getFullYear())</script> kelvinGichunge Softwares</h5>
+        <h5>Email : kimgichunge6@gmail.com </h5>
+        <h5>Phone : +254 724097929</h5>
+        
+        <h5>"With Software Creation,All the World's Problems are Solved ! "</h5>
+   </div>
+</BODY>
+</HTML>
